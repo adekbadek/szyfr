38a1d3223f39f549d48e72b9e299f8d3a49f91d0
Adam Borowski <adam@adamboro.com>
2016-01-20 16:07:09 +0100

    Keys handling

--- js/Game.jsx
+++ js/Game.jsx
@@ -3,9 +3,26 @@ import ReactDOM from "react-dom"
 
 import Component from './Component';
 
+let isLetter = (keyCode) => {
+  return (65 <= keyCode && keyCode <= 90);
+}
+
 class Game extends React.Component{
   constructor(props){
     super(props)
+  }
+  handleKeyup(e){
+    let {keyCode} = e;
+
+    // return if it's not a letter 
+    if(!isLetter(keyCode)) {
+      return;
+    }
+
+    console.log(String.fromCharCode(e.keyCode));
+  }
+  componentDidMount() {
+    window.addEventListener("keyup",this.handleKeyup.bind(this))
   }
 	render() {
 		return (
